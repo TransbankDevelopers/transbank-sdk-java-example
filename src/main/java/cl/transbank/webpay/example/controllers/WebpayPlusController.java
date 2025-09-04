4e3901ff98e1ee4d582e57e0b8b8cf8c4350baf9
Miguel Villalobos <miguel.villalobos@continuumhq.com>
2025-09-04 18:01:24 -0400

    feat: add commit method to controller

--- src/main/java/cl/transbank/webpay/example/controllers/WebpayPlusController.java
+++ src/main/java/cl/transbank/webpay/example/controllers/WebpayPlusController.java
@@ -5,12 +5,17 @@ import cl.transbank.common.IntegrationCommerceCodes;
 import cl.transbank.common.IntegrationType;
 import cl.transbank.webpay.common.WebpayOptions;
 import cl.transbank.webpay.webpayplus.WebpayPlus;
+import cl.transbank.webpay.webpayplus.responses.WebpayPlusTransactionCommitResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -64,6 +69,73 @@ public class WebpayPlusController extends BaseController {
             log.error("ERROR", e);
         }
         return "webpay_plus/create";
+    }
+
+    @RequestMapping(value = "/commit", method = { RequestMethod.GET, RequestMethod.POST })
+    public String commit(
+            @RequestParam(name = "token_ws", required = false) String tokenWs,
+            @RequestParam(name = "TBK_TOKEN", required = false) String tbkToken,
+            @RequestParam(name = "TBK_ORDEN_COMPRA", required = false) String tbkBuyOrder,
+            @RequestParam(name = "TBK_ID_SESION", required = false) String tbkSessionId,
+            Model model) {
+
+        Map<String, String> navigation = new LinkedHashMap<>() {
+            {
+                put("data", "Paso 1 - Datos recibidos");
+                put("request", "Paso 2 - Petición");
+                put("response", "Paso 3 - Respuesta");
+                put("operations", "¡Listo!");
+            }
+        };
+        model.addAttribute("navigation", navigation);
+
+        Map<String, String> breadcrumbs = new LinkedHashMap<>() {
+            {
+                put("Inicio", "/");
+                put("Webpay Plus", "/webpay_plus/create");
+                put("Confirmar transacción", "#");
+            }
+        };
+        model.addAttribute("breadcrumbs", breadcrumbs);
+
+        Map<String, Object> details = new HashMap<>();
+        model.addAttribute("details", details);
+
+        // Flujo de transacción abortada por el usuario
+        if (tbkToken != null) {
+            details.put("tbkToken", tbkToken);
+            details.put("buyOrder", tbkBuyOrder);
+            details.put("sessionId", tbkSessionId);
+            return "webpay_plus/aborted";
+        }
+
+        // Flujo de timeout
+        if (tokenWs == null) {
+            details.put("buyOrder", tbkBuyOrder);
+            details.put("sessionId", tbkSessionId);
+            return "webpay_plus/timeout";
+        }
+
+        // Flujo normal
+        log.info(String.format("token_ws : %s", tokenWs));
+        details.put("token_ws", tokenWs);
+
+        try {
+            final WebpayPlusTransactionCommitResponse response = tx.commit(tokenWs);
+            log.debug(String.format("response : %s", response));
+
+            details.put("response", response);
+            details.put("amount", (int) response.getAmount());
+            details.put("resp", toJson(response));
+            details.put("refund-endpoint", "/webpay_plus/refund");
+            details.put("status-endpoint", "/webpay_plus/status");
+
+        } catch (Exception e) {
+            log.error("ERROR", e);
+            details.put("resp", e.getMessage());
+        }
+
+        return "webpay_plus/commit";
     }
 
 }
