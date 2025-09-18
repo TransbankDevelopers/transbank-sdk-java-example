9f2c7a8c7206f4c9fe147ad3bc682657372dbab9
mvarlic <mvarlic@gmail.com>
2025-09-18 15:16:33 -0500

    feat: add error page constants

--- src/main/java/cl/transbank/webpay/example/controllers/BaseController.java
+++ src/main/java/cl/transbank/webpay/example/controllers/BaseController.java
@@ -1,15 +1,21 @@
 package cl.transbank.webpay.example.controllers;
 
 import com.google.gson.GsonBuilder;
+
 import java.util.Random;
 
-public  abstract class BaseController {
+public abstract class BaseController {
 
-    public String toJson(Object obj){
+    protected static final String VIEW_ERROR = "error/error_page";
+    protected static final String VIEW_ABORTED_ERROR = "error/aborted";
+    protected static final String VIEW_FORM_ERROR = "error/form_page";
+    protected static final String VIEW_TIMEOUT_ERROR = "error/timeout";
+
+    public String toJson(Object obj) {
         return (new GsonBuilder().setPrettyPrinting().create()).toJson(obj);
     }
 
-    protected String getRandomNumber(){
+    protected String getRandomNumber() {
         return String.valueOf(new Random().nextInt(Integer.MAX_VALUE));
     }
 }
