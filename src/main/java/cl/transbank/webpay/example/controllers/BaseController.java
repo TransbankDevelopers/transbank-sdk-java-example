0da0323fd328732f830df8b4ffde0f95f43a1c99
mvarlic <mvarlic@gmail.com>
2025-09-18 16:06:05 -0500

    feat: change page name

--- src/main/java/cl/transbank/webpay/example/controllers/BaseController.java
+++ src/main/java/cl/transbank/webpay/example/controllers/BaseController.java
@@ -8,7 +8,7 @@ public abstract class BaseController {
 
     protected static final String VIEW_ERROR = "error/error_page";
     protected static final String VIEW_ABORTED_ERROR = "error/aborted";
-    protected static final String VIEW_FORM_ERROR = "error/form_page";
+    protected static final String VIEW_FORM_ERROR = "error/form_error";
     protected static final String VIEW_TIMEOUT_ERROR = "error/timeout";
 
     public String toJson(Object obj) {
